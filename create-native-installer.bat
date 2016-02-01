9eed73e2093039ebcd2a64f4152b4f6e969bcd0d
Edvin Syse <es@syse.no>
2016-02-01 13:05:03 +0100

    Added deploy and installer scripts

---
+++ create-native-installer.bat
@@ -0,0 +1,9 @@
+@echo off
+
+mkdir tmp
+mkdir tmp\lib
+cd tmp\lib
+wget http://repo1.maven.org/maven2/no/tornado/fxlauncher/1.0.1/fxlauncher-1.0.1.jar
+cd ..
+javapackager -deploy -native -outdir packages -outfile FXSamples -srcdir . -srcfiles lib/fxlauncher-1.0.1.jar -argument http://fxsamples.tornado.no/app.xml -appclass fxlauncher.Launcher -name "FXSamples" -title "FXSamples"
+
