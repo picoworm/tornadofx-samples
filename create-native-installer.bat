11d89b7890911045e0bc197fe8b6ea02e3f10bfe
Edvin Syse <es@syse.no>
2016-02-01 13:39:51 +0100

    Added license file to installers

--- create-native-installer.bat
+++ create-native-installer.bat
@@ -1,9 +1,13 @@
 @echo off
 
-mkdir tmp
-mkdir tmp\lib
-cd tmp\lib
+#!/usr/bin/env bash
+
+mkdir tmp/
+cd tmp
+wget http://www.apache.org/licenses/LICENSE-2.0.txt
+mkdir lib
+cd lib
 wget http://repo1.maven.org/maven2/no/tornado/fxlauncher/1.0.1/fxlauncher-1.0.1.jar
 cd ..
-javapackager -deploy -native -outdir packages -outfile FXSamples -srcdir . -srcfiles lib/fxlauncher-1.0.1.jar -argument http://fxsamples.tornado.no/app.xml -appclass fxlauncher.Launcher -name "FXSamples" -title "FXSamples"
+javapackager -deploy -native -outdir packages -outfile FXSamples -srcdir . -srcfiles lib/fxlauncher-1.0.1.jar:LICENSE-2.0.txt -argument http://fxsamples.tornado.no/app.xml -appclass fxlauncher.Launcher -name "FXSamples" -title "FXSamples" -BlicenseFile=LICENSE-2.0.txt
 cd ..
--- create-native-installer.sh
+++ create-native-installer.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 mkdir -p tmp/lib
-cd tmp/lib
+cd tmp
+wget http://www.apache.org/licenses/LICENSE-2.0.txt
+cd lib
 wget http://repo1.maven.org/maven2/no/tornado/fxlauncher/1.0.1/fxlauncher-1.0.1.jar
 cd ..
-javapackager -deploy -native -outdir packages -outfile FXSamples -srcdir . -srcfiles lib/fxlauncher-1.0.1.jar -argument http://fxsamples.tornado.no/app.xml -appclass fxlauncher.Launcher -name "FXSamples" -title "FXSamples"
+javapackager -deploy -native -outdir packages -outfile FXSamples -srcdir . -srcfiles lib/fxlauncher-1.0.1.jar:LICENSE-2.0.txt -argument http://fxsamples.tornado.no/app.xml -appclass fxlauncher.Launcher -name "FXSamples" -title "FXSamples" -BlicenseFile=LICENSE-2.0.txt
 cd ..
