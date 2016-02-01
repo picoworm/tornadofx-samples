8248697e02cc2bbe2dafe9f81cfb56fbf6f1f5ba
Edvin Syse <es@syse.no>
2016-02-01 13:42:06 +0100

    Added license file to installers

--- create-native-installer.bat
+++ create-native-installer.bat
@@ -2,12 +2,12 @@
 
 #!/usr/bin/env bash
 
-mkdir tmp/
+mkdir tmp
 cd tmp
 wget http://www.apache.org/licenses/LICENSE-2.0.txt
 mkdir lib
 cd lib
 wget http://repo1.maven.org/maven2/no/tornado/fxlauncher/1.0.1/fxlauncher-1.0.1.jar
 cd ..
-javapackager -deploy -native -outdir packages -outfile FXSamples -srcdir . -srcfiles lib/fxlauncher-1.0.1.jar:LICENSE-2.0.txt -argument http://fxsamples.tornado.no/app.xml -appclass fxlauncher.Launcher -name "FXSamples" -title "FXSamples" -BlicenseFile=LICENSE-2.0.txt
+javapackager -deploy -native -outdir packages -outfile FXSamples -srcdir . -srcfiles lib/fxlauncher-1.0.1.jar;LICENSE-2.0.txt -argument http://fxsamples.tornado.no/app.xml -appclass fxlauncher.Launcher -name "FXSamples" -title "FXSamples" -BlicenseFile=LICENSE-2.0.txt
 cd ..
